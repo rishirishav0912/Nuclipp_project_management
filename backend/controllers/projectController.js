9e1663090b2ec62b21cac57de30222bff3da8a54
rishirishav0912 <rishirishav912@gmail.com>
2024-11-08 12:54:25 +0530

    changes-in-logic

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -181,7 +181,7 @@ const payProject = async (req, res) => {
 
         console.log("checksum", checkSum);
 
-        const prodUrl = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay";
+        const prodUrl = "https://api.phonepe.com/apis/hermes/pg/v1/pay";
         const options = {
             method: 'POST',
             headers: {
